a65586838207bed4f9f740e61568b677b4971d2a
William Coffey <WilliamCoffeyTri@gmail.com>
2018-02-25 20:58:03 -0500

    Implemented the actual ping, and an output report. Added flags for the amount of pings sent and the timeout.

--- pingsweep.py
+++ pingsweep.py
@@ -1,20 +1,23 @@
 import argparse
 import re
 import sys
+import subprocess
+import os
 
 parser = argparse.ArgumentParser(description='Ping sweeps an IP range')
+parser.add_argument('-c', '--count', type=int, default=4, help='Number of pings to send to each host.')
+parser.add_argument('-w', '--timeout', type=int, help='Time to wait for a response from each host.')
 parser.add_argument('range', type=str, help='String in the format [lower_bound]-[upper_bound] or in CIDR notation representing the range to sweep.')
 
+args = parser.parse_args()
 
 def main():
-    args = parser.parse_args()
     ipRange = compileRange(args.range)
     if(ipRange == None):
         parser.print_help()
         sys.exit(1)
 
-    print(ipRange)
-    liveAddresses = sweepRange(ipRange)
+    report(sweepRange(ipRange))
 
 
 def compileRange(iprange):
@@ -39,17 +42,37 @@ def compileRange(iprange):
 def sweepRange(iprange):
     addresses = []
 
+    output = open(os.devnull, 'w')
+
     for a in range(iprange[0][0], iprange[1][0]+1):
         for b in range(iprange[0][1], iprange[1][1]+1):
             for c in range(iprange[0][2], iprange[1][2]+1):
                 for d in range(iprange[0][3], iprange[1][3]+1):
                     ip = str(a) + "." + str(b) + "." + str(c) + "." + str(d)
-                    if(ping(ip)):
-                        addresses.append(ip)                 
+                    print("Querying Address: " + ip)
+                    if(ping(ip, output)):
+                        addresses.append(ip)             
+
+    return addresses
 
 
-def ping(ip):
-    print(ip)
+def report(liveAddresses):
+    print("The following addresses on the range responded: ")
+    for address in liveAddresses:
+        print(address)
+
+
+def ping(ip, of):
+    command = ['ping']
+
+    command.append('-c')
+    command.append(str(args.count))
+    
+    if not args.timeout == None:
+        command.append('-w')
+        command.append(str(args.timeout))
+    command.append(ip)
+    return not subprocess.call(command, stdout=of, stderr=of) 
 
 
 if __name__ == "__main__":
